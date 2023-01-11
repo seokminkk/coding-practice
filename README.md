a446872b08fc5ecbcfeba1fc8394cedb7f9a3983
seokmin <seokmin0016@gmail.com>
2023-01-12 00:07:24 +0900

    230111

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # coding-practice
 
+나중에 다시보면 좋을것<br>
+230111 <br>
+
+<br><br><br><br>
+
 221219/filter를 이용한 분류, 짝수더하기,letof,tilt연산자<br>
 230110/객체와 스프레드 map을 이용한 풀이<br>
 230111/shift,unshift,Math.max를 잘이용하자<br>
--- level0/202301-2/20230111.ts
+++ level0/202301-2/20230111.ts
@@ -1,6 +1,6 @@
 // 정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
 
-function solution(array = [1, 3, 5, 9]) {
+function solution(array: Array<number>) {
   let max = Math.max(...array);
 
   return [max, array.indexOf(max)];
